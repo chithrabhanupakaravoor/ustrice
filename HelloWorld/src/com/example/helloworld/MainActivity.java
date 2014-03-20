6ed877d0fa594552a1a5105f2e87bc91eb4ccd06
smh <smh@955f8dec-2ffc-f99b-d367-aad65c597513>
2014-03-20 08:36:54 +0000

    testing

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -7,9 +7,9 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
-
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,11 +25,16 @@ public class MainActivity extends Activity {
 
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
-	    Intent intent = new Intent(this, DisplayMessageActivity.class);
-	    EditText editText = (EditText) findViewById(R.id.edit_message);
-	    String message = editText.getText().toString();
-	    intent.putExtra(EXTRA_MESSAGE, message);
-	    startActivity(intent);
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
-}
 
+	public void nextPage(View view) {
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		startActivity(intent);
+	}
+
+}
