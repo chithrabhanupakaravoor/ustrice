d6c609e2cf58f513337d48e1fb64f1270b0e8ba0
smhchai <smhchai@955f8dec-2ffc-f99b-d367-aad65c597513>
2014-03-21 20:37:00 +0000

    Basic Interface(with icons)

---
+++ UST_RICE/src/com/example/ust_rice/Account.java
@@ -0,0 +1,104 @@
+package com.example.ust_rice;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.os.Build;
+
+public class Account extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_account);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	ImageButton mbFood, mbRecommend, mbRanking, mbFavourites;
+
+	public void goFood(View view) {
+		Intent i = new Intent(this, FoodMenu.class);
+		mbFood = (ImageButton) findViewById(R.id.mbFood);
+		i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		startActivity(i);
+		finish();
+		overridePendingTransition(0, 0);
+	}
+
+	public void goRecommend(View view) {
+		Intent i = new Intent(this, Recommend.class);
+		mbRecommend = (ImageButton) findViewById(R.id.mbRecommend);
+		i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		startActivity(i);
+		finish();
+		overridePendingTransition(0, 0);
+	}
+
+	public void goRanking(View view) {
+		Intent i = new Intent(this, Ranking.class);
+		mbRanking = (ImageButton) findViewById(R.id.mbRanking);
+		i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		startActivity(i);
+		finish();
+		overridePendingTransition(0, 0);
+	}
+
+	public void goFavourites(View view) {
+		Intent i = new Intent(this, Favourites.class);
+		mbFavourites = (ImageButton) findViewById(R.id.mbFavourites);
+		i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		startActivity(i);
+		finish();
+		overridePendingTransition(0, 0);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.account, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_account,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
