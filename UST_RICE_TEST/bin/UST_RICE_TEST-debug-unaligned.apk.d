a7368cd5977ab8c4ad1c2015d6fa83f32553e280
smhchai <smhchai@955f8dec-2ffc-f99b-d367-aad65c597513>
2014-05-07 15:12:45 +0000

    DELETE

--- UST_RICE_TEST/bin/UST_RICE_TEST-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Users\SMH\Desktop\adt-bundle-windows-x86-20131030\workspace\UST_RICE_TEST\bin\UST_RICE_TEST-debug-unaligned.apk : \
-C:\Users\SMH\Desktop\adt-bundle-windows-x86-20131030\workspace\UST_RICE_TEST\bin\UST_RICE_TEST.ap_ \
-C:\Users\SMH\Desktop\adt-bundle-windows-x86-20131030\workspace\UST_RICE_TEST\bin\classes.dex \
-C:\Users\SMH\Desktop\adt-bundle-windows-x86-20131030\workspace\UST_RICE_TEST\libs\android-junit-report-1.5.8.jar \
-C:\Users\SMH\Desktop\adt-bundle-windows-x86-20131030\workspace\UST_RICE_TEST\libs\android-support-v4.jar \
